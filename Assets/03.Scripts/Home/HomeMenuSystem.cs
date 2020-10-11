dc8ba86ab7889b8b3a13ce7d19d79778fa757b0e
JiWonHan <acsang12@gmail.com>
2020-10-11 22:04:17 +0900

    HomeTestScene 버튼 테스트 작업

---
+++ Assets/03.Scripts/Home/HomeMenuSystem.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HomeMenuSystem : MonoBehaviour
+{
+
+    public void GoGameStart()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("IntroScene");
+    }
+    public void GoExitGame()
+    {
+        Application.Quit();
+    }
+    public void GoSetting()
+    {
+
+    }
+}
