95d929938ebb95199d1979b35140992e9596687e
JiWonHan <acsang12@gmail.com>
2020-10-11 22:33:20 +0900

    홈 로고 움직임 추가

--- Assets/03.Scripts/Home/HomeMenuSystem.cs
+++ Assets/03.Scripts/Home/HomeMenuSystem.cs
@@ -4,7 +4,14 @@ using UnityEngine;
 
 public class HomeMenuSystem : MonoBehaviour
 {
+    public GameObject logoObj;
 
+    public float speed;
+    public float scaleVal;
+    void Update()
+    {
+        logoObj.gameObject.transform.localScale = new Vector3(Mathf.PingPong(Time.time*speed, scaleVal)+1, Mathf.PingPong(Time.time*speed, scaleVal)+1, 1);
+    }
     public void GoGameStart()
     {
         UnityEngine.SceneManagement.SceneManager.LoadScene("IntroScene");
